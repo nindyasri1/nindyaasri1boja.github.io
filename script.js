352198b21621a8d48cb5b972e133fc10e3989590
nindyasri1 <kingsmart.psl@gmail.com>
2026-01-12 17:31:01 +0700

    Update script.js
    Signed-off-by: nindyasri1 <kingsmart.psl@gmail.com>

--- script.js
+++ script.js
@@ -1,47 +1,3 @@
-let data = JSON.parse(localStorage.getItem("keuanganRT")) || [];
-
-const form = document.getElementById("formKeuangan");
-const tabel = document.getElementById("tabelData");
-const saldoText = document.getElementById("saldo");
-
-form.addEventListener("submit", function(e) {
-    e.preventDefault();
-
-    const item = {
-        tanggal: tanggal.value,
-        jenis: jenis.value,
-        jumlah: parseInt(jumlah.value),
-        keterangan: keterangan.value
-    };
-
-    data.push(item);
-    localStorage.setItem("keuanganRT", JSON.stringify(data));
-
-    form.reset();
-    tampilkanData();
-});
-
-function tampilkanData() {
-    tabel.innerHTML = "";
-    let saldo = 0;
-
-    data.forEach(d => {
-        saldo += d.jenis === "masuk" ? d.jumlah : -d.jumlah;
-
-        tabel.innerHTML += `
-            <tr>
-                <td>${d.tanggal}</td>
-                <td>${d.jenis}</td>
-                <td>Rp ${d.jumlah.toLocaleString()}</td>
-                <td>${d.keterangan}</td>
-            </tr>
-        `;
-    });
-
-    saldoText.innerText = `Saldo: Rp ${saldo.toLocaleString()}`;
-}
-
-tampilkanData();
 function simpanKeGoogle() {
     const data = {
         tanggal: document.getElementById("tanggal").value,
@@ -66,4 +22,3 @@ function simpanKeGoogle() {
         console.error(err);
     });
 }
-
